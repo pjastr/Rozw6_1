efabcc9a5085aa7255763a9d309816e24b6cf981
Piotrek <@>
2017-11-15 18:53:00 +0100

    przeslanie plików

---
+++ Rozw_6_1/Osoba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_6_1
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+
+        public Osoba()
+        {
+
+        }
+
+        public Osoba(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public virtual void WypiszInfo()
+        {
+            Console.WriteLine("Imię i nazwisko: {0} {1}", imie, nazwisko);
+        }
+    }
+}
---
+++ Rozw_6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba student1 = new Student("Jan", "Kowalski",1,2,34567);
+            student1.WypiszInfo();
+
+
+            Console.ReadKey();
+        }
+    }
+}
